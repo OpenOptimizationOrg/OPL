e41e48c88dd36c137b652d8af9a97e1cf8ea96ff
Dvermetten <d.vermetten@gmail.com>
2026-02-11 10:51:05 +0100

    Avoid duplicates and empty optional fields

--- formresponse_to_yaml.py
+++ formresponse_to_yaml.py
@@ -53,18 +53,36 @@ data = pd.read_csv(csv_file)
 data = data.fillna("")
 data_main = data.rename(columns=translations)
 data_main.drop(columns=[col for col in data_main.columns if col not in translations.values()], inplace=True)
-data_main = data_main[data_main['name'] != 'test']
 data_other = data.rename(columns=translations_other)
 data_other.drop(columns=[col for col in data_other.columns if col not in translations_other.values()], inplace=True)
-data_other = data_other[data_other['name'] != 'test']
 
 dict_main = data_main.to_dict(orient="records")
 dict_other = data_other.to_dict(orient="records")
 for dict_m, dict_o in zip(dict_main, dict_other):
-    dict_o['name'] = None  # Remove duplicate name entry
+    for k in list(dict_o.keys()):
+        if dict_o[k] is None or dict_o[k] == "":
+            dict_o.pop(k)
+    dict_o.pop("name")  # Remove duplicate name entry
     dict_m['other info'] = dict_o
 
-# Write the yaml file
-with open(yaml_file, "a") as out_file:
-    yaml.dump(dict_main, out_file,
-              sort_keys=False)  # Prevent columns being reordered alphabetically
+with open(yaml_file) as in_file:
+    data = pd.json_normalize(yaml.safe_load(in_file))
+existing_names = [d['name'] for d in data] + ['test']
+
+#Filter out entries already in the yaml file
+dict_new = []
+for d in dict_main:
+    name = d['name']
+    if name not in existing_names:
+        dict_new.append(d)
+
+if len(dict_new) > 0:
+
+    print(f"Number of new entries to add: {len(dict_new)}")
+
+    # Write the yaml file
+    with open(yaml_file, "a") as out_file:
+        yaml.dump(dict_new, out_file,
+                sort_keys=False)  # Prevent columns being reordered alphabetically
+else:
+    print("No new entries to add.")
