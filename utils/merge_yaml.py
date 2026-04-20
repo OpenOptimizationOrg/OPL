88092bc11dfc2c4bdd20f76219c846b46ce98cf8
TheHedgeify <thehedgeify@gmail.com>
2026-04-20 14:29:29 +0200

    tested script now

--- utils/merge_yaml.py
+++ utils/merge_yaml.py
@@ -7,13 +7,13 @@ from pathlib import Path
 parent = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(parent))
 
-from .validate_yaml import read_data, validate_data, PROBLEMS_FILE
+from utils.validate_yaml import read_data, validate_data, PROBLEMS_FILE
 
 
 def write_data(filepath, data):
     try:
         with open(filepath, "w") as f:
-            yaml.safe_dump(data, f)
+            yaml.safe_dump(data, f, sort_keys=False)
         print(f"::notice::Wrote data to {filepath}.")
     except FileNotFoundError:
         print(f"::error::File not found: {filepath}")
@@ -25,8 +25,7 @@ def write_data(filepath, data):
 
 
 def update_existing_data(existing_data, new_data):
-    existing_data.update(new_data)
-
+    existing_data.extend(new_data)
     write_success = write_data(PROBLEMS_FILE, existing_data)
     return write_success
 
@@ -45,15 +44,22 @@ def delete_new_file(file_path):
 def merge_new_problems(new_problems_yaml_path: str):
     # Read and validate new data
     new_data_status, new_data = read_data(new_problems_yaml_path)
-    if not new_data_status:
+    if new_data_status != 0:
+        print(
+            f"::error::New problems data could not be read from {new_problems_yaml_path}."
+        )
         return False
     valid = validate_data(new_data)
     if not valid:
+        print(f"::error::New problems data in {new_problems_yaml_path} is not valid.")
         return False
 
     # Read existing data
     existing_data_status, existing_data = read_data(PROBLEMS_FILE)
-    if not existing_data_status:
+    if existing_data_status != 0:
+        print(
+            f"::error::Existing problems data could not be read from {PROBLEMS_FILE}."
+        )
         return False
 
     # All valid, we can now just merge the dicts
@@ -61,11 +67,13 @@ def merge_new_problems(new_problems_yaml_path: str):
     assert new_data is not None
     updated = update_existing_data(existing_data, new_data)
     if not updated:
+        print(f"::error::Failed to update existing problems data in {PROBLEMS_FILE}.")
         return False
 
     # Reset the template content
     reset_status = delete_new_file(new_problems_yaml_path)
     if not reset_status:
+        print(f"::error::Failed to delete new problem file {new_problems_yaml_path}.")
         return False
 
     print(f"::notice::Merged {len(new_data)} new problems into {PROBLEMS_FILE}.")
