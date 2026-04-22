32c8b8681f6511a93a654598cc0f568d2b6f7d7b
Dvermetten <d.vermetten@gmail.com>
2026-04-22 14:17:31 +0200

    Change links to main branch and add if__main__ things

--- yaml_to_html.py
+++ yaml_to_html.py
@@ -56,86 +56,88 @@ html_scripts = f"{html_dir}javascript.html"
 html_footer = f"{html_dir}footer.html"
 html_index = f"{html_dir}index.html"
 html_table_template = f"{html_dir}table_template.html"
-
-# Load data
-with open(yaml_file) as yaml_input:
-    raw_data = pd.json_normalize(yaml.safe_load(yaml_input))
-
-if "problem_id" in raw_data.columns:
-    problem_ids = raw_data["problem_id"].fillna("").map(str)
-else:
-    problem_ids = raw_data["name"].fillna("").map(str).map(to_problem_id)
-
-problem_ids = problem_ids.where(problem_ids.str.len() > 0, raw_data["name"].fillna("").map(str).map(to_problem_id))
-data = raw_data.copy()
-
-# Choose desired columns
-all_columns = False
 default_columns = ["name",
-                   "textual description",
-                   "suite/generator/single",
-                   "objectives",
-                   "dimensionality",
-                   "variable type",
-                   "constraints",
-                   "dynamic",
-                   "noise",
-                   "multi-fidelity",
-                   "source (real-world/artificial)",
-                   "reference",
-                   "implementation"]
+                "textual description",
+                "suite/generator/single",
+                "objectives",
+                "dimensionality",
+                "variable type",
+                "constraints",
+                "dynamic",
+                "noise",
+                "multi-fidelity",
+                "source (real-world/artificial)",
+                "reference",
+                "implementation"]
 
-if all_columns is False:
-    columns = default_columns
-    data = data[columns]
+if __name__ == "__main__":
 
-data = data.map(linkify_cell)
+    # Load data
+    with open(yaml_file) as yaml_input:
+        raw_data = pd.json_normalize(yaml.safe_load(yaml_input))
 
-# Generate plain table
-table = data.to_html(render_links=False,
-                     escape=False,  # Don't escape HTML in cells (to allow links)
-                     index=False,
-                     table_id="problems",
-                     classes=["display compact", "display", "styled-table"],  # Set display style
-                     border=0,
-                     na_rep="")  # Leave NaN cells empty
+    if "problem_id" in raw_data.columns:
+        problem_ids = raw_data["problem_id"].fillna("").map(str)
+    else:
+        problem_ids = raw_data["name"].fillna("").map(str).map(to_problem_id)
 
-table = add_problem_id_row_attributes(table, problem_ids.tolist())
+    problem_ids = problem_ids.where(problem_ids.str.len() > 0, raw_data["name"].fillna("").map(str).map(to_problem_id))
+    data = raw_data.copy()
 
-# Add footer to facilitate individual column search
-idx = table.index('</table>')
-final_table = table[:idx] + "<tfoot><tr>" + " ".join(["<th>"+ i +"</th>" for i in data.columns])+"</tr> </tfoot>" + table[idx:]
+    # Choose desired columns
+    all_columns = False
 
-default_hidden_columns = {"textual description", "reference", "implementation"}
+    if all_columns is False:
+        columns = default_columns
+        data = data[columns]
 
-column_toggles = "".join(
-    [
-        (
-            f'<label class="column-chip">'
-            f'<input class="col-toggle" type="checkbox" data-column="{i}"'
-            f'{" checked" if col not in default_hidden_columns else ""}>'
-            f'<span>{escape(col)}</span>'
-            f'</label>'
-        )
-        for i, col in enumerate(data.columns)
-    ]
-)
+    data = data.map(linkify_cell)
 
-with open(html_table_template, encoding="utf-8") as template_file:
-    table_template = template_file.read()
+    # Generate plain table
+    table = data.to_html(render_links=False,
+                        escape=False,  # Don't escape HTML in cells (to allow links)
+                        index=False,
+                        table_id="problems",
+                        classes=["display compact", "display", "styled-table"],  # Set display style
+                        border=0,
+                        na_rep="")  # Leave NaN cells empty
 
-table_markup = (
-    table_template
-    .replace("__COLUMN_TOGGLES__", column_toggles)
-    .replace("__TABLE__", final_table)
-)
+    table = add_problem_id_row_attributes(table, problem_ids.tolist())
 
-# Write table to file
-with open(html_table, "w", encoding="utf-8") as table_file:
-    table_file.write(table_markup)
+    # Add footer to facilitate individual column search
+    idx = table.index('</table>')
+    final_table = table[:idx] + "<tfoot><tr>" + " ".join(["<th>"+ i +"</th>" for i in data.columns])+"</tr> </tfoot>" + table[idx:]
 
-# Merge table and scripts into HTML page
-with open(html_index, "wb") as output_file:
-    for part_path in [html_header, html_table, html_scripts, html_footer]:
-        with open(part_path, "rb") as part_file:
-            shutil.copyfileobj(part_file, output_file)
+    default_hidden_columns = {"textual description", "reference", "implementation"}
+
+    column_toggles = "".join(
+        [
+            (
+                f'<label class="column-chip">'
+                f'<input class="col-toggle" type="checkbox" data-column="{i}"'
+                f'{" checked" if col not in default_hidden_columns else ""}>'
+                f'<span>{escape(col)}</span>'
+                f'</label>'
+            )
+            for i, col in enumerate(data.columns)
+        ]
+    )
+
+    with open(html_table_template, encoding="utf-8") as template_file:
+        table_template = template_file.read()
+
+    table_markup = (
+        table_template
+        .replace("__COLUMN_TOGGLES__", column_toggles)
+        .replace("__TABLE__", final_table)
+    )
+
+    # Write table to file
+    with open(html_table, "w", encoding="utf-8") as table_file:
+        table_file.write(table_markup)
+
+    # Merge table and scripts into HTML page
+    with open(html_index, "wb") as output_file:
+        for part_path in [html_header, html_table, html_scripts, html_footer]:
+            with open(part_path, "rb") as part_file:
+                shutil.copyfileobj(part_file, output_file)
